52ef36cfc1d9c1c350401bacb2173e09984fa6f7
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2026-01-07 21:16:44 +0530

    config was upadated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true
   },
-});
+
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+      util: 'util',
+      events: 'events'
+    }
+  },
+
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
+
+  define: {
+    'process.env': {}
+  }
+})
